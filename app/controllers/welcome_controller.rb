9ab5d355f3eb946b44f646d218eb4fc9a60c8d50
Tim Millar <heteroskedasticity99@gmail.com>
2019-05-26 23:01:46 +0100

    chapter 13, production deploy on a local vm

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,8 @@ class WelcomeController < ApplicationController
   def index
     redis = Redis.new(host: 'redis', port: 6379)
     redis.incr "page hits"
+
     @page_hits = redis.get "page hits"
+    @hostname = Socket.gethostname
   end
 end
